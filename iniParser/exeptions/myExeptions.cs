e147ed22eac5acc642a789c67a6686d6725996b2
DAGERON <55352282+dageron20@users.noreply.github.com>
2020-10-22 02:57:34 +0300

    finish fix

---
+++ iniParser/exeptions/myExeptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iniParser.exeptions
+{
+    class iniExeptions : System.Exception
+    {
+        public iniExeptions(string message) : base(message) { }
+
+        public class InvalidFormat : iniExeptions
+        {
+            public InvalidFormat(string message)
+                : base("Error. Format is not .ini") { }
+        }
+
+        public class InvalidParametr : iniExeptions
+        {
+            public InvalidParametr(string message)
+                : base("Invalid parameter type") { }
+        }
+    }
+
+
+}
+
