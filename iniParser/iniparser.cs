88c3a24c6eb260e55fe7892b4208d4717f686bb0
DAGERON <55352282+dageron20@users.noreply.github.com>
2020-10-26 22:54:18 +0300

    fix: nesting

--- iniParser/iniparser.cs
+++ iniParser/iniparser.cs
@@ -21,73 +21,65 @@ namespace iniParser
             string strLine;
             string currentMain = null;
             string[] keyPair;
-
             pathToFile = pathfile;
-
-            if (File.Exists(pathToFile))
+            if (!File.Exists(pathToFile))
+                throw new FileNotFoundException("Path not found " + pathfile);
+            try
             {
-                try
+                TextReader file = new StreamReader(pathToFile);
+                var extension = Path.GetExtension(pathToFile); // получаем расширени файла
+                if (extension != ".ini")
                 {
-                    TextReader file = new StreamReader(pathToFile);
-                    var extension = Path.GetExtension(pathToFile); // получаем расширени файла
-                    if (extension != ".ini")
+                    throw new InvalidFormat("Error format"); // ошибка неверного расширение файла
+                }
+                strLine = file.ReadLine();
+                while (strLine != null)
+                {
+                    strLine = strLine.Trim(); // удаляет все начальные и конечные символы пробела из текущей строки
+                    /*if (strLine == "")
                     {
-                        throw new InvalidFormat("Error format"); // ошибка неверного расширение файла
+                        strLine = file.ReadLine();  Возможно бесмысленная конструкция из за того что мы в while и так проверяем не нулевую строку
+                        continue;
+                    }*/
+                    if (strLine.StartsWith("[") && strLine.EndsWith("]")) // проверка на название [Секции]
+                    {
+                        currentMain = strLine.Substring(1, strLine.Length - 2); // считываем название
                     }
                     else
                     {
-                        strLine = file.ReadLine();
-                        while (strLine != null)
+                        if (strLine.StartsWith(";"))
                         {
-                            strLine = strLine.Trim(); // удаляет все начальные и конечные символы пробела из текущей строки
-                            if (strLine == "")
-                            {
-                                strLine = file.ReadLine();
-                                continue;                               
-                            }
-                            if (strLine.StartsWith("[") && strLine.EndsWith("]")) // проверка на название [Секции]
-                            {
-                                currentMain = strLine.Substring(1, strLine.Length - 2); // считываем название
-                            }
-                            else
-                            {
-                                if (strLine.StartsWith(";"))
-                                {
-                                    // строка = коммент   
-                                    strLine = file.ReadLine();
-                                    continue;
-                                }
-                                strLine = strLine.Replace(" ", string.Empty); // удаляем пробелы 
-                                keyPair = strLine.Split(new char[] { '=' }, 2); // разделяем строку на 2 подстроки разделенные знаком равно
-                                DataPairs datapairs;
-                                string value = null;
-                                if (currentMain == null)
-                                {
-                                    continue;                                        
-                                }
-                                datapairs.section = currentMain;
-                                datapairs.key = keyPair[0];
-                                if (keyPair.Length > 1)
-                                {
-                                    keyPair[1] = Regex.Replace(keyPair[1], @";.+$", string.Empty); // удаляем комментарий, если он находится в строке
-                                    value = keyPair[1];
-                                }
-                                if (pairsParameters[datapairs] != null)
-                                    ChangeParameter(datapairs.section, datapairs.key, value);
-                                else
-                                    pairsParameters.Add(datapairs, value);
-                            }
+                            // строка = коммент   
                             strLine = file.ReadLine();
+                            continue;
                         }
+                        strLine = strLine.Replace(" ", string.Empty); // удаляем пробелы 
+                        keyPair = strLine.Split(new char[] { '=' }, 2); // разделяем строку на 2 подстроки разделенные знаком равно
+                        DataPairs datapairs;
+                        string value = null;
+                        if (currentMain == null)
+                        {
+                            continue;
+                        }
+                        datapairs.section = currentMain;
+                        datapairs.key = keyPair[0];
+                        if (keyPair.Length > 1)
+                        {
+                            keyPair[1] = Regex.Replace(keyPair[1], @";.+$", string.Empty); // удаляем комментарий, если он находится в строке
+                            value = keyPair[1];
+                        }
+                        if (pairsParameters[datapairs] != null)
+                            ChangeParameter(datapairs.section, datapairs.key, value);
+                        else
+                            pairsParameters.Add(datapairs, value);
                     }
-                }
-                catch (iniExeptions ex)
-                {
-                    throw ex;
+                    strLine = file.ReadLine();
                 }
             }
-            else
-                throw new FileNotFoundException("Path not found " + pathfile);
+            catch (iniExeptions ex)
+            {
+                throw ex;
+            }
         }
 
         public string ReadData(string section, string setting)
@@ -99,10 +91,7 @@ namespace iniParser
             {
                 throw new InvalidParametr("Section or parametr not found");
             }
-            else
-            {
-                return (string)pairsParameters[datapairs];
-            }
+            return (string)pairsParameters[datapairs];
         }
 
         public void ChangeParameter(string section, string setting, string value)
@@ -128,16 +117,11 @@ namespace iniParser
             {
                 throw new InvalidParametr("Section or parametr not found");
             }
-            else
-            {
-                string s = pairsParameters[datapairs].ToString();
-                bool valid = int.TryParse(s, out int number);
-                if (!valid)
-                    throw new InvalidParametr("Invalid format to Parse");
-                else
-                    return number;
-            }
-
+            string s = pairsParameters[datapairs].ToString();
+            bool valid = int.TryParse(s, out int number);
+            if (!valid)
+                throw new InvalidParametr("Invalid format to Parse");
+            return number;
         }
         public double TakeDouble(string section, string setting)
         {
@@ -148,15 +132,11 @@ namespace iniParser
             {
                 throw new InvalidParametr("Section or parametr not found");
             }
-            else
-            {
-                string s = pairsParameters[datapairs].ToString();
-                bool valid = double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out double number);
-                if (!valid)
-                    throw new InvalidParametr("Invalid format to Parse");
-                else
-                    return number;
-            }
+            string s = pairsParameters[datapairs].ToString();
+            bool valid = double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out double number);
+            if (!valid)
+                throw new InvalidParametr("Invalid format to Parse");
+            return number;
         }
     }
 }
